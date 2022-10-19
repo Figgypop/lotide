1f7601a13ffb9d8e4e3721282059eac06b9bec98
David Figueroa <figueroadavid93@gmail.com>
2022-10-19 16:02:23 -0400

    Count Letters function - turn a sentence into object that gives count for each letter

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+
+//The function should take in a sentence (as a string) 
+//and then return an object with the count of each of the 
+//letters in that sentence. 
+const countLetters = function (sentence) {
+  const result = {}
+
+  //Iterate through each value of string
+  for (let letter of sentence) {
+    if (letter === " ") {
+      continue;
+    }
+
+    //console.log(letter);  --> test code
+    if (result[letter] == undefined) {
+      result[letter] = 0
+    }
+
+    result[letter]++;
+
+  }
+
+  //return values as object
+  return result;
+}
+
+console.log(countLetters("doesnt matter"));
