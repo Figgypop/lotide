4badd7d3d52a7207a7125ef52d17e2d42948f155
David Figueroa <figueroadavid93@gmail.com>
2022-10-18 22:02:29 -0400

    Implement eqArrays Lotide problem

--- tail.js
+++ tail.js
@@ -7,11 +7,10 @@ const assertEqual = function (actual, expected) {
   return;
 };
 
-const tail = function(array) {
+const tail = function (array) {
   return array.slice(1);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
