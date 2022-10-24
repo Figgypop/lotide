c6f9a98a1d54a43a464da6208ac30b0a7dcb52ff
David Figueroa <figueroadavid93@gmail.com>
2022-10-24 10:22:28 -0400

    middleTest updated to run with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,24 @@
-const assertArrayEqual = require('../assertArrayEqual');
-const eqArrays = require('../eqArrays');
-const middle = require('../middle')
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), 2);
+  });
+
+  it("returns [1, 2] for [0, 1, 2, 3]", () => {
+    assert.deepEqual(middle([0, 1, 2, 3]), [1, 2]);
+  });
+});
 
 
-console.log(middle([]));            // undefined
-console.log(middle([0]));           // 
-console.log(middle([0, 1]));         // []
-console.log(middle([0, 1, 2]));       // 1
-console.log(middle([0, 1, 2, 3]));     // [1, 2]
-console.log(middle([0, 1, 2, 3, 4]));   // 2
+
+// const assertArrayEqual = require('../assertArrayEqual');
+// const eqArrays = require('../eqArrays');
+// const middle = require('../middle')
+// console.log(middle([]));            // undefined
+// console.log(middle([0]));           // 
+// console.log(middle([0, 1]));         // []
+// console.log(middle([0, 1, 2]));       // 1
+// console.log(middle([0, 1, 2, 3]));     // [1, 2]
+// console.log(middle([0, 1, 2, 3, 4]));   // 2 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns ['Wassa', 'Suh'] for ['Yo Yo', 'Wassa', 'Suh']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Wassa', 'Suh']), ['Wassa', 'Suh']);
+    
+  })
+});
+
+
+
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements! 
