765b9dc3aa24d4f5fcac5a61c1c3fb2f97f38ae5
David Figueroa <figueroadavid93@gmail.com>
2022-11-01 19:06:42 -0400

    assertObjectEqual function completed to compare two objects and return an easy to read pass or fail

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,5 @@
 const assertObjectsEqual = function (actual, expected) {
-  if (!eqObjects(actual === expected)) {
+  if (!eqObjects(actual, expected)) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   } else {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,17 +7,6 @@ const assertObjectsEqual = function (actual, expected) {
   return;
 };
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 
 //function that takes in two objects
 const eqObjects = function (object1, object2) {
@@ -33,39 +22,21 @@ const eqObjects = function (object1, object2) {
 
   // compare values of keys
   for (let key of object1Keys) {
-    const object1Value = object1[key] // ["2", 3]
-    const object2Value = object2[key] // ["2", 3, 4]
-    console.log(object1Value);
-    console.log(object2Value);
-
-    // Check if value is an array and if they are equal
-    if ((Array.isArray(object1Value) && Array.isArray(object2Value)) && !eqArrays(object1Value, object2Value)) {
-      return false;
+    if (object1[key] !== object2[key]) {
+      return false
     }
 
   }
-
-  if (object1Value !== object2Value) {
-    return false;
-  }
-
   return true;
 }
 
-assertObjectsEqual();
+// assertObjectsEqual();
 
-const cd = {
-  c: "1",
-  d: ["2", 3]
-};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
 
-const dc = {
-  d: ["2", 3],
-  c: "1"
-};
 
-const cd2 = {
-  c: "1",
-  d: ["2", 3, 4]
-};
+// const abc = { a: "1", b: "2", c: "3" };
+// assertObjectsEqual(eqObjects(ab, abc), false); // => false
 
